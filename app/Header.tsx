fdd5f836700d876169528b22cc98c1b86aecab9d
fudhoil <fudhoil.10119333@mahasiswa.unikom.ac.id>
2022-11-13 15:57:15 +0700

    no middleware

--- app/Header.tsx
+++ app/Header.tsx
@@ -50,6 +50,11 @@ const Header = ({ session }: any) => {
             </p>
           </div>
         </div>
+        <Link
+          href="/api/auth/signin"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Sign In
+        </Link>
       </header>
     );
   }
